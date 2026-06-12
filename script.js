a50ccad8e50d45682a385e3628081df351dfac26
Oliver <114B15585@mailst.cjcu.edu.tw>
2026-06-12 10:25:02 +0800

    v2 ATOMIC WIND FIELD

--- script.js
+++ script.js
@@ -1,108 +1,120 @@
-let particles = [];
-let obstacles = [];
-let flowZ = 0;
+let particles = []
+let obstacles = []
+let running = false
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 420; i++) {
-    particles.push(new Particle());
+  createCanvas(windowWidth, windowHeight)
+
+  for (let i = 0; i < 1000; i++) {
+    particles.push(new Particle())
+  }
+
+  let btn = document.getElementById("startBtn")
+  btn.onclick = function () {
+    document.getElementById("overlay").style.display = "none"
+    running = true
   }
 }
 
 function draw() {
-  background(0, 14);
+  background(5, 10, 20, 25)
 
-  flowZ += 0.003;
+  if (!running) return
 
-  for (let p of particles) {
-    p.update();
-    p.show();
+  for (let o of obstacles) {
+    noStroke()
+    fill(120, 180, 255, 160)
+    circle(o.x, o.y, o.r * 2)
   }
 
-  noStroke();
-  fill(255, 70);
-  for (let o of obstacles) {
-    circle(o.x, o.y, o.r * 2);
+  for (let p of particles) {
+
+    let wind = createVector(10, 0)
+
+    let noiseY = noise(p.pos.y * 0.002, frameCount * 0.01)
+    let wobble = map(noiseY, 0, 1, -2, 2)
+
+    wind.y = wobble
+
+    wind.mult(1)
+
+    p.applyForce(wind)
+    p.update()
+    p.edges()
+    p.show()
+
+    for (let o of obstacles) {
+      p.collide(o)
+    }
   }
 }
 
 function mousePressed() {
+  if (!running) return
+
   obstacles.push({
     x: mouseX,
     y: mouseY,
-    r: 65
-  });
+    r: random(6, 14)
+  })
 }
 
 function keyPressed() {
-  if (key === 'r' || key === 'R') {
-    obstacles = [];
+  if (key === "r" || key === "R") {
+    obstacles = []
   }
-}
-
-function startExperience() {
-  document.getElementById("landing").style.display = "none";
 }
 
 class Particle {
   constructor() {
-    this.pos = createVector(random(width), random(height));
-    this.prev = this.pos.copy();
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
+    this.pos = createVector(random(0, width * 0.1), random(height))
+    this.vel = createVector(0, 0)
+    this.acc = createVector(0, 0)
+    this.maxSpeed = 12
+  }
+
+  applyForce(f) {
+    this.acc.add(f)
   }
 
   update() {
-
-    let angle = noise(
-      this.pos.x * 0.004,
-      this.pos.y * 0.004,
-      flowZ
-    ) * TWO_PI * 4;
-
-    let wind = p5.Vector.fromAngle(angle);
-    wind.mult(1.2);
-    this.acc.add(wind);
-
-    for (let o of obstacles) {
-      let d = dist(this.pos.x, this.pos.y, o.x, o.y);
-
-      if (d < o.r) {
-
-        let normal = createVector(this.pos.x - o.x, this.pos.y - o.y);
-        normal.normalize();
-
-        this.pos.x = o.x + normal.x * o.r;
-        this.pos.y = o.y + normal.y * o.r;
-
-        let v = this.vel.copy();
-        let dot = v.dot(normal);
-
-        let reflect = p5.Vector.sub(v, p5.Vector.mult(normal, 2 * dot));
-
-        this.vel = reflect;
-        this.vel.mult(0.75);
-      }
-    }
-
-    this.vel.mult(0.97);
-    this.vel.add(this.acc);
-    this.vel.limit(4);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-
-    this.edges();
+    this.vel.add(this.acc)
+    this.vel.limit(this.maxSpeed)
+    this.pos.add(this.vel)
+    this.acc.mult(0)
   }
 
   edges() {
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
+    if (this.pos.x > width) this.pos.x = 0
+    if (this.pos.y < 0) this.pos.y = height
+    if (this.pos.y > height) this.pos.y = 0
   }
 
+    collide(o) {
+    let d = dist(this.pos.x, this.pos.y, o.x, o.y)
+
+    if (d < o.r + 2) {
+
+        let dir = createVector(this.pos.x - o.x, this.pos.y - o.y)
+
+        let strength = map(d, 0, o.r, 10, 2)
+
+        dir.setMag(strength)
+
+        this.vel.add(dir)
+
+        this.vel.mult(0.6)
+
+        this.pos.add(dir.mult(0.8))
+    }
+    }
+
   show() {
-    stroke(255, 150);
-    point(this.pos.x, this.pos.y);
+    stroke(180, 220, 255, 110)
+    point(this.pos.x, this.pos.y)
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
 }
